96ba1a842783c5037f0305381034128d72611c8c
Rafael Hirooka <rafaelhirooka16@gmail.com>
2020-07-07 23:40:39 -0300

    card white image and border

--- components/CardWhite/index.js
+++ components/CardWhite/index.js
@@ -1,9 +1,29 @@
 import styles from 'CardWhite.module.css';
 
-export default function CardWhite() {
+export default function CardWhite({title, description, image, button: Button}) {
   return (
     <div className={styles.card}>
+      <div className={styles.cardTitleMobile}>
+        <h2>{title}</h2>
+      </div>
 
+      <div className={styles.cardContent}>
+        <div className={styles.cardBody}>
+          <h2>{title}</h2>
+
+          <p>{description}</p>
+
+          {Button || null}
+        </div>
+      </div>
+
+      <div className={styles.cardImageBorder}>
+        <div className={styles.cardImageBorderContent}></div>
+      </div>
+
+      <div className={styles.cardImage}>
+        <img src={image} alt={title} />
+      </div>
     </div>
   );
 }
